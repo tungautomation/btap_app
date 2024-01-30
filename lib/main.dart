4b99eb40cfbd3d1de9df419687e5e180cbfe12eb
tungautomation <bkv253@gmail.com>
2024-01-30 20:28:15 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: CounterPage(""),
     );
   }
